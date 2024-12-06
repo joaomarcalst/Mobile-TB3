d766f393f93bcd00f64519dad4df566c1de76f11
joaomarcalst <jpstorino@gmail.com>
2024-12-06 14:21:46 +0100

    RoomActivity creation

---
+++ AUTOMACORP/app/src/main/java/com/example/automacorp/RoomActivity.kt
@@ -0,0 +1,67 @@
+package com.example.automacorp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.automacorp.ui.theme.AutomacorpTheme
+
+class RoomActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AutomacorpTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    RoomDetail(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RoomDetail(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var nameState by remember { mutableStateOf(name) }
+        Text(
+            text = stringResource(R.string.act_room_name),
+            style = MaterialTheme.typography.labelSmall,
+            modifier = Modifier.padding(bottom = 4.dp)
+        )
+        OutlinedTextField(
+            nameState,
+            onValueChange = {
+                nameState = it
+            },
+            placeholder = { Text(stringResource(R.string.act_room_name)) },
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RoomDetailPreview() {
+    AutomacorpTheme {
+        RoomDetail("Android")
+    }
+}
