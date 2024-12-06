7f0a74fa1c6ada6c433cf578aa21145895fe2ec6
joaomarcalst <jpstorino@gmail.com>
2024-12-06 14:11:51 +0100

    update android UI app

--- AUTOMACORP/app/src/main/java/com/example/automacorp/MainActivity.kt
+++ AUTOMACORP/app/src/main/java/com/example/automacorp/MainActivity.kt
@@ -6,18 +6,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -31,16 +38,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val onButtonClick: () -> Unit = {
-            Toast.makeText(baseContext, "Hello button", Toast.LENGTH_LONG).show()
+        val onSayHelloButtonClick: (name: String) -> Unit = { name ->
+            Toast.makeText(baseContext, "Hello $name", Toast.LENGTH_LONG).show()
         }
 
         setContent {
             AutomacorpTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        "Android",
-                        onClick = onButtonClick,
+                        onClick = onSayHelloButtonClick,
                         modifier = Modifier.padding(innerPadding),
                     )
                 }
@@ -50,8 +56,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
-    Column {
+fun AppLogo(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.ic_logo),
+        contentDescription = stringResource(R.string.app_logo_description),
+        modifier = modifier
+            .paddingFromBaseline(top = 100.dp)
+            .height(80.dp),
+    )
+}
+
+@Composable
+fun Greeting(onClick: (name: String) -> Unit, modifier: Modifier = Modifier) {
+    var textFieldValue by remember { mutableStateOf("") }
+
+    Column(modifier = modifier) {
         AppLogo(Modifier.padding(top = 32.dp).fillMaxWidth())
         Text(
             stringResource(R.string.act_main_welcome),
@@ -61,6 +80,30 @@ fun Greeting(name: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
                 .align(Alignment.CenterHorizontally),
             textAlign = TextAlign.Center,
         )
+        OutlinedTextField(
+            value = textFieldValue,
+            onValueChange = { newValue ->
+                textFieldValue = newValue
+                println("value of the field : $newValue")
+            },
+            modifier = Modifier.padding(24.dp).fillMaxWidth(),
+            placeholder = {
+                Row {
+                    Icon(
+                        imageVector = Icons.Rounded.AccountCircle,
+                        modifier = Modifier.padding(end = 8.dp),
+                        contentDescription = stringResource(R.string.act_main_fill_name),
+                    )
+                    Text(stringResource(R.string.act_main_fill_name))
+                }
+            }
+        )
+        Button(
+            onClick = { onClick(textFieldValue) }, // Passa o valor correto
+            modifier = Modifier.padding(8.dp).align(Alignment.CenterHorizontally)
+        ) {
+            Text(stringResource(R.string.act_main_open))
+        }
     }
 }
 
@@ -69,18 +112,8 @@ fun Greeting(name: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     AutomacorpTheme {
         Greeting(
-            "Android",
-            onClick = TODO(),
-            modifier = TODO()
+            onClick = { name -> println("Button clicked with name: $name") },
+            modifier = Modifier
         )
     }
-}
-
-@Composable
-fun AppLogo(modifier: Modifier) {
-    Image(
-        painter = painterResource(R.drawable.ic_logo),
-        contentDescription = stringResource(R.string.app_logo_description),
-        modifier = modifier.paddingFromBaseline(top = 100.dp).height(80.dp),
-    )
 }
